fe31f7e7227de9fa2f3c99e5954eeecf581716c0
Rafael <132296553+R4f4ell@users.noreply.github.com>
2024-09-23 17:58:03 -0300

    Site quase completo

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,31 +1,44 @@
-// Seleciona o ícone de menu e a navegação
-const menuIcon = document.createElement('div');
-menuIcon.innerHTML = '&#9776;'; // Símbolo de hambúrguer
-menuIcon.classList.add('menu-icon');
+class MobileNavBar{
+    constructor(mobileMenu, navList, navLinks){
+        this.mobileMenu = document.querySelector(mobileMenu)
+        this.navList = document.querySelector(navList)
+        this.navLinks = document.querySelectorAll(navLinks)
+        this.activeClass = 'active'
 
-// Insere o ícone no header
-const header = document.querySelector('header');
-header.appendChild(menuIcon);
-
-// Evento de clique para alternar o menu
-menuIcon.addEventListener('click', () => {
-    const nav = document.querySelector('header nav');
-    
-    // Alterna a visibilidade do menu
-    if (nav.style.display === 'flex') {
-        nav.style.display = 'none';
-    } else {
-        nav.style.display = 'flex';
+        this.handleClick = this.handleClick.bind(this)
     }
-});
 
-// Função para garantir que o menu volte a ficar visível em telas maiores
-window.addEventListener('resize', () => {
-    const nav = document.querySelector('header nav');
-    
-    if (window.innerWidth > 768) {
-        nav.style.display = 'flex'; // Mostra o menu em telas maiores
-    } else {
-        nav.style.display = 'none'; // Esconde o menu em telas menores
+    handleClick(){
+        this.navList.classList.toogle(this.activeClass)
+        this.animateLinks()
     }
-});
+
+    animateLinks(){
+        this.navLinks.forEach((link) => {
+            link.style.animation
+            ? (link.style.animation = "")
+            : (link.style.animation = "navLinkFade 0.5s ease forwards 0.3s")
+        })
+    }
+
+    addClickEvent(){
+        this.mobileMenu.addEventListener("click", this.handleClick)
+    }
+
+    init(){
+        if(this.mobileMenu){
+            this.addClickEvent()
+        }
+
+        return this;
+    }
+
+}
+
+const mobileNavBar = new MobileNavBar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+);
+
+mobileNavBar.init()
